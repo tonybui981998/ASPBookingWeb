d56bdf330ee774cce5d7afc03edb25227b433a9a
TonyBui <buianh981998@gmail.com>
2024-12-04 21:41:55 +1300

    create table

---
+++ BookingWebApp.Infrastructure/Migrations/20241204083154_ModifyNameInVillaTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingWebApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyNameInVillaTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UpdateDate",
+                table: "Villas",
+                newName: "Update_Date");
+
+            migrationBuilder.RenameColumn(
+                name: "CreateDate",
+                table: "Villas",
+                newName: "Create_Date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Update_Date",
+                table: "Villas",
+                newName: "UpdateDate");
+
+            migrationBuilder.RenameColumn(
+                name: "Create_Date",
+                table: "Villas",
+                newName: "CreateDate");
+        }
+    }
+}
